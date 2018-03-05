bf708ab091c9c3e371cf7c92b12cbf9bad100c78
MrFlyingbananas <mrflyingbananas@yahoo.com>
2018-03-04 21:09:48 -0600

    Code reformating

--- challenges/subsets-add-up-to-n/add_up.py
+++ challenges/subsets-add-up-to-n/add_up.py
@@ -1,12 +1,12 @@
 def add_up(li, n):
     left = 0
-    right = len(li)-1
+    right = len(li) - 1
     count = 0
     while left < right:
-        if li[left]+li[right] == n:
+        if li[left] + li[right] == n:
             count += 1
             right -= 1
-        elif li[left]+li[right] > n:
+        elif li[left] + li[right] > n:
             right -= 1
         else:
             left += 1
